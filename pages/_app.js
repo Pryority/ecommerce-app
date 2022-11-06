5337a03b5f2aabcb49d2e6f12bc1d38d6d45b65a
pryority <matthewapryor@gmail.com>
2022-11-06 13:44:22 -0500

    add query client imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from '../lib/query';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
