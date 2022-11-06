9391ade271878d22ca5e2d240bfdddea24abca62
pryority <matthewapryor@gmail.com>
2022-11-06 13:54:16 -0500

    add components dir, Navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return(
+    <div className="relative bg-white mx-6">
+      <div className="flex items-center justify-between pt-6 md:justify-start md:space-x-10">
+        <div className="flex justify-start lg:w-0 lg:flex-1">
+          <h1 className="text-2xl">
+            <Link href="/" className="cursor-pointer">
+                            Ecomm App
+            </Link>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
