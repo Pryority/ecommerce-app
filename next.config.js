a7a91459b74126a5f49fc1d54f855bb559df404c
pryority <matthewapryor@gmail.com>
2022-11-07 16:07:29 -0500

    change next.config.js back to normal

--- next.config.js
+++ next.config.js
@@ -3,11 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    loader: 'akamai',
-    path: '',
     domains: ['placeimg.com'],
   },
-  assetPrefix: './',
 };
 
 module.exports = nextConfig;
